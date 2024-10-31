78bdddf2d21e1be470b64688185bcf3eca6cf661
sanghoo1 <np510@naver.com>
2024-10-31 10:19:19 +0900

    241024 수업

---
+++ javascriptBasic.js
@@ -0,0 +1,5 @@
+document.querySelector('body').innerHTML = 'hello JavaScript !';
+
+// Object.method(parameter); 객체.메서드(매개변수);
+console.log('메시지 출력');// 콘솔 영역에 출력
+console.clear(); // 거의 쓰진 않는다. 콘솔창에 내용 지우기
